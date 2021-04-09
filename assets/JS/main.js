60febf27885b0bacf4b6a2af088189801b771bca
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-09 18:15:39 -0500

    styled with css

---
+++ assets/JS/main.js
@@ -0,0 +1,8 @@
+const timerEl = document.getElementById('timer')
+const questionEl = document.getElementById('question')
+const countdownEl = document.getElementById('timer')
+const answersEl = document.getElementById('question-list')
+
+function timer() {
+    var timeLeft = 60
+}
