f0a03279bd4bf83a3428c4a591568d6deda2d1ec
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-15 02:05:27 -0500

    countdown timer working

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,8 +1,23 @@
-const countdownTimerEl = document.getElementById('countdown-timer')
+// Dom Elements
+const timeleftEl = document.querySelector('.countdown-timer')
 const questionEl = document.getElementById('question')
-const countdownEl = document.getElementById('timer')
-const answersEl = document.getElementById('answer-list')
+const answerListEl = document.getElementById('answer-list')
+const answerEL = document.getElementsByClassName('answer')
+const startBtnEl = document.querySelector('#start-btn')
 
-function timer() {
-    var timeLeft = 60
-}
+
+// countdown timer function
+function timerCountdown() {
+
+    let timeLeft = 60
+
+    var timeInterval = setInterval(function(){
+        if (timeLeft >= 0) {
+            timeleftEl.textContent = timeLeft
+            timeLeft--
+        }
+        else {clearInterval(timeInterval)}
+    },1000)}
+
+// Start Game button
+startBtnEl.addEventListener('click', timerCountdown)
