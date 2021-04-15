d81f59b6ef0208970c3dedb67b0052835d6f9305
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-15 00:10:29 -0500

    updated comments for html

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,7 @@
-const timerEl = document.getElementById('timer')
+const countdownTimerEl = document.getElementById('countdown-timer')
 const questionEl = document.getElementById('question')
 const countdownEl = document.getElementById('timer')
-const answersEl = document.getElementById('question-list')
+const answersEl = document.getElementById('answer-list')
 
 function timer() {
     var timeLeft = 60
