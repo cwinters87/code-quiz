88ffd291fb10f4717b499db491b93fcc4ae8374d
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-15 13:30:40 -0500

    developed buttons and start button

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,22 +2,66 @@
 const timeleftEl = document.querySelector('.countdown-timer')
 const questionEl = document.getElementById('question')
 const answerListEl = document.getElementById('answer-list')
-const answerEL = document.getElementsByClassName('answer')
+const answerOneEL = document.querySelector('#answer-one')
+const answerTwoEL = document.querySelector('#answer-two')
+const answerThreeEL = document.querySelector('#answer-three')
+const answerFourEL = document.querySelector('#answer-four')
 const startBtnEl = document.querySelector('#start-btn')
 
+// time and score count
+let timeLeft = 60
+let score = 0
+
+function hello() {
+    alert('howdy')
+}
 
 // countdown timer function
 function timerCountdown() {
-
-    let timeLeft = 60
-
     var timeInterval = setInterval(function(){
-        if (timeLeft >= 0) {
+    if (timeLeft >= 0) {
             timeleftEl.textContent = timeLeft
             timeLeft--
-        }
-        else {clearInterval(timeInterval)}
-    },1000)}
+    }
+    else {clearInterval(timeInterval)}        
+    },
+    1000)
+}
+
+
+
+
+
+
+
 
 // Start Game button
-startBtnEl.addEventListener('click', timerCountdown)
+startBtnEl.addEventListener('click', e => {
+    timerCountdown()
+    startBtnEl.remove()
+})
+
+// answer event listeners
+answerOneEL.addEventListener('click', e => {
+    score++
+    hello()
+    console.log(score)
+})
+
+answerTwoEL.addEventListener('click', e => {
+    score++
+    hello()
+    console.log(score)
+})
+
+answerThreeEL.addEventListener('click', e => {
+    score++
+    hello()
+    console.log(score)
+})
+
+answerFourEL.addEventListener('click', e => {
+    score++
+    hello()
+    console.log(score)
+})  
